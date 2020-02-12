30f8aca9425b8606cd5425c8e5930f58f9ff72bd
tastaba <tt10589@georgiasouthern.edu>
2020-02-12 12:14:08 -0500

    Add files via upload

---
+++ awsRekog.py
@@ -0,0 +1,50 @@
+import boto3
+import json
+from boto.s3.connection import S3Connection
+import config
+
+def detect_multipleimage_faces(bucketName, connection):
+    bucket = connection.get_bucket(bucketName)
+    resultRows= []
+    for image in bucket.list():
+        resultRows.append(detect_faces(image.name,bucketName))
+    print('Here are the Emotions:')
+    for r in resultRows:
+        print(r)
+    print(len(resultRows))
+    writeToCsv(resultRows)
+
+def detect_faces(photo, bucket):
+    client = boto3.client('rekognition')
+    response = client.detect_faces(Image={'S3Object':{'Bucket':bucket,'Name':photo}},Attributes=['ALL'])
+    #print('Detected faces for ' + photo)
+    csvList= []
+    csvList.append(photo)
+    for faceDetail in response['FaceDetails']:
+        result = json.dumps(faceDetail['Emotions'], indent=4, sort_keys=True)
+        # create a JSON object parser; the result is a key-value pair dictionary
+        parsed = json.loads(result)
+        emotionDict = dict()
+        for line in parsed:
+            emotionDict[line['Type']] = line['Confidence']
+        emotionDictKeys = sorted(emotionDict.keys())
+        for e in emotionDictKeys:
+            csvList.append(emotionDict[e])
+        return csvList
+
+def writeToCsv(rowsList):
+    import csv
+    with open('awsEmotion.csv', 'a') as csvFile:
+        writer = csv.writer(csvFile)
+        writer.writerows(rowsList)
+        csvFile.close()
+
+def main():
+    bucket = '1polar'
+    access_key_ID = config.aws_access_key_ID
+    secret_access_key = config.aws_secret_access_key
+    conn = S3Connection(access_key_ID, secret_access_key)
+    detect_multipleimage_faces(bucket, conn)
+
+if __name__ == "__main__":
+    main()
